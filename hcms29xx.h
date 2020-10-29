f569da78fe697b6142820d25b0f2e90cbb036585
Michael Bieri <mb@alonso.ch>
2020-10-29 22:20:30 +0100

    Backported new version from https://github.com/PaulStoffregen/LedDisplay

--- hcms29xx.h
+++ hcms29xx.h
@@ -1,18 +1,12 @@
 /*
-  hcms29xx -- controller library for Avago HCMS-297x displays -- version 0.4
+  LedDisplay -- controller library for Avago HCMS-297x displays -- version 0.3
   27 Jan 2010 ML: extended a bit to support up to four (4) 8 character displays.
-  15 May 2014 AW: alterred setBrightness() to use 50% peak pixel current, and support cascaded displays.
-  10 Nov 2014 AW: Moved the dotRegister[] array to be a public variable, this way we manipulate the pixels directly.
-  05 Dec 2014 AW: Made the dotRegister[] array size a "#define" to conserve precious SRAM (see below this comment block - line 42).
- 
+  
    Copyright (c) 2009 Tom Igoe. Some right reserved.
  
    Revisions on version 0.2 and 0.3 by Mark Liebman, 27 Jan 2010
     * extended a bit to support up to four (4) 8 character displays.
-   
-   Revisions on version 0.4 by Andrew Wyatt, 15 May 2014
-    * alterred setBrightness() to use 50% peak pixel current, and support cascaded displays.
- 
+  vim: set ts=4:
    
   Controls an Avago HCMS29xx display. This display has 8 characters, each 5x7 LEDs
    
@@ -34,17 +28,19 @@
   * Version 3 adds support for multiple displays.  Just change the displayLength to 
     the total number of characters (for example, 3 8-character displays, displayLength = 24).
 
-  * Version 5 alters the setBrightness method for cascaded displays. It also sets the peak pixel current to 50%.
 */
 
 
-// We need one byte per pixel column. Each character is 5 pixels wide, so we need 5 bytes per character.
-#define DOT_REGISTER_BYTES 40 // 40 bytes = 12 characters.
-
- 
-// ensure this library description is only included once 
+// ensure this library description is only included once
 #ifndef hcms29xx_h
-#define hcms29xx_h 
+#define hcms29xx_h
+
+// supports up to four 8 character displays, connected as documented here,
+// under "Multiple Displays" http://playground.arduino.cc/Main/LedDisplay
+#define LEDDISPLAY_MAXCHARS  32
+
+// Setup dotregister
+#define DOT_REGISTER_BYTES 40
 
 // include types & constants of Wiring core API
 #if ARDUINO >= 100
@@ -53,78 +49,74 @@
 #include "WProgram.h"
 #endif
 
-// Pascal Stang's 5x7 font library:
-#include "font5x7.h"
-// The font library is stored in prgram memory:
-#include <avr/pgmspace.h>
-// string functions used to manipulate strings:
-#include <string.h>
-
-// Arduino Print library provides print() and println() functionality 
+// Arduino Print library provides print() and println() functionality
 #include "Print.h"
+
+// Add font lib
+#include "font5x7.h"
 
 // library interface description
 
 class hcms29xx : public Print {
   public:
     // constructor:
-   hcms29xx(uint8_t _dataPin, 
-            uint8_t _registerSelect, 
-            uint8_t _clockPin, 
-            uint8_t _chipEnable, 
-            uint8_t _resetPin, 
-            uint8_t _displayLength);
-   
+   hcms29xx(uint8_t _dataPin,
+			  uint8_t _registerSelect,
+			  uint8_t _clockPin,
+			  uint8_t _chipEnable,
+			  uint8_t _resetPin,
+			  uint8_t _displayLength);
 
-    // initializer method:
-    void begin();
-    
-    void clear();                       // clear the display
-    void home();                        // set cursor to far left hand position
-    void setCursor(int whichPosition);  // set cursor to any position
-    int  getCursor();                   // get the cursor position
+
+	// initializer method:
+	void begin();
+
+	void clear();						// clear the display
+	void home();						// set cursor to far left hand position
+	void setCursor(int whichPosition);	// set cursor to any position
+	int  getCursor();					// get the cursor position
 
 #if ARDUINO >= 100
-    virtual size_t write(uint8_t b);    // write a character to the display and advance cursor
+	virtual size_t write(uint8_t b);	// write a character to the display and advance cursor
 #else
-    virtual void write(uint8_t b);      // write a character to the display and advance cursor
+	virtual void write(uint8_t b);		// write a character to the display and advance cursor
 #endif
-    using Print::write;
-    
-    void setString(char* _stringToDisplay);     // set the displayString variable
-    char* getString();                          // get the displayString
-    int stringLength();                         // get the length of displayString
+	using Print::write;
 
-    void scroll(int direction);         // scroll whatever string is stored in library's displayString variable
-    
-    void setBrightness(uint8_t bright);         // set display brightness, 0 - 15
+	void setString(const char* _stringToDisplay);	// set the displayString variable
+	const char * getString();						// get the displayString
+	int stringLength();							// get the length of displayString
 
-    // Control register setters. for addressing the display directly:
-    void loadControlRegister(int dataByte);
-    void loadDotRegister();
-            
-    int version(void);                          // return library version
+	void scroll(int direction);			// scroll whatever string is stored in library's displayString variable
 
-    
-    // I've moved the dot register to be a public variable - Andrew 2014-11-10
+	void setBrightness(uint8_t bright);			// set display brightness, 0 - 15
+
+	// Control register setters. for addressing the display directly:
+	void loadControlRegister(uint8_t dataByte);
+	void loadAllControlRegisters(uint8_t dataByte);
+	void loadDotRegister();
+
+    int version(void);							// return library version
+
     uint8_t dotRegister[DOT_REGISTER_BYTES];
 
-    // I've moved the writeCharacter function to be public - Andrew 2014-11-11
-    // Character display setters:
-    void writeCharacter(char whatCharacter, byte whatPosition); // write a character to a buffer which will
-                                                                // be sent to the display by loadDotRegister()
   private:
+  	// Character display setters:
+	void writeCharacter(char whatCharacter, byte whatPosition);	// write a character to a buffer which will
+																// be sent to the display by loadDotRegister()
 
-    int cursorPos;              // position of the cursor       
+	int cursorPos;				// position of the cursor
+	//uint8_t dotRegister[LEDDISPLAY_MAXCHARS * 5]; // 5 bytes per character * maxchars
 
-    // Define pins for the LED display:
-    uint8_t  dataPin;           // connects to the display's data in
-    uint8_t registerSelect;     // the display's register select pin 
-    uint8_t clockPin;           // the display's clock pin
-    uint8_t chipEnable;         // the display's chip enable pin
-    uint8_t resetPin;           // the display's reset pin
-    uint8_t displayLength;      // number of bytes needed to pad the string
-    char* displayString;        // string for scrolling
+	// Define pins for the LED display:
+	uint8_t dataPin;         	// connects to the display's data in
+	uint8_t registerSelect;   	// the display's register select pin
+	uint8_t clockPin;         	// the display's clock pin
+	uint8_t chipEnable;       	// the display's chip enable pin
+	uint8_t resetPin;         	// the display's reset pin
+	uint8_t displayLength;    	// number of bytes needed to pad the string
+	char stringBuffer[LEDDISPLAY_MAXCHARS+1];  // buffer to hold initial display string
+	const char * displayString;	// string for scrolling
 };
 
 #endif
